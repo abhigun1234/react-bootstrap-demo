45875eda21e45d4b2b617dc95a5177be34a0ef0d
Abhishek <abhi.ghosh27feb@gmail.com>
2022-12-17 20:24:47 +0530

    product

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+// rsf for  functional 
+function Header(props) {
+    return (
+        <div>
+                 <Navbar bg="light" expand="lg">
+                        <Container>
+                            <Navbar.Brand href="#home">Ecomerce</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#link">Products</Nav.Link>
+                                <Nav.Link href="#link">AboutUs</Nav.Link>
+                                <Nav.Link href="#link">Contact Us</Nav.Link>
+                            </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+        </div>
+    );
+}
+
+export default Header;
